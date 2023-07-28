9f0c8a6937d282188853888209e6269c043f21cf
Müller <106887962+LMuller89@users.noreply.github.com>
2023-07-28 15:42:41 -0300

    Criação dos bilboards

---
+++ HEC_Core/HEC_Billboard/config.cpp
@@ -0,0 +1,44 @@
+class CfgPatches
+{
+	class HEC_Billboard
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data",
+			"DZ_Structures_Signs"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class HEC_Billboard_1: HouseNoDestruct
+	{
+		scope=2;
+		model="billboard\HEC_Billboard_1.p3d";
+	};
+	class HEC_Billboard_2: HouseNoDestruct
+	{
+		scope=2;
+		model="billboard\HEC_Billboard_2.p3d";
+	};
+	class HEC_Billboard_3: HouseNoDestruct
+	{
+		scope=2;
+		model="billboard\HEC_Billboard_3.p3d";
+	};
+	class HEC_Billboard_4: HouseNoDestruct
+	{
+		scope=2;
+		model="billboard\HEC_Billboard_4.p3d";
+	};
+	class HEC_Billboard_5: HouseNoDestruct
+	{
+		scope=2;
+		model="billboard\HEC_Billboard_5.p3d";
+	};
+};
