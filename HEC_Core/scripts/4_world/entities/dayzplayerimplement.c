85f4695b6c479e40963daa4f20f77ae13e408d49
Müller <106887962+LMuller89@users.noreply.github.com>
2024-03-01 00:34:44 -0300

    BILLBOARD RETIRADO ADMIN

---
+++ HEC_Core/scripts/4_world/entities/dayzplayerimplement.c
@@ -0,0 +1,18 @@
+modded class DayZPlayerImplement extends DayZPlayer
+{
+// MOVIMENTO INCLINADO COMO ERA ANTES
+	void OnMovementChanged()//(int state_old, int state_new)
+	{
+
+	}
+	
+	void OnSprintStart()
+	{
+
+	}
+	
+	void OnSprintEnd()
+	{
+
+	}
+}
